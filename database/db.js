710e0b524913e34c9259372962b8ef416b094bf5
hakimnazry <hakimnazry@gmail.com>
2024-04-05 03:40:51 +0800

    use env var for port

--- database/db.js
+++ database/db.js
@@ -3,10 +3,9 @@ const sqlite3 = require("sqlite3");
 const db = new sqlite3.Database("./products.db");
 
 db.serialize(() => {
-    db.run(
-        `CREATE TABLE IF NOT EXISTS Product(id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, serial_number INTEGER NOT NULL, isBoycott INTEGER NOT NULL)`
-      );
+  db.run(
+    `CREATE TABLE IF NOT EXISTS Product(id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, serial_number INTEGER NOT NULL, isBoycott INTEGER NOT NULL)`
+  );
 });
-
 
 module.exports = db;
--- database/util.js
+++ database/util.js
@@ -1,3 +1,4 @@
 require("./lib/deleteOneProduct");
 require("./lib/readAllProducts");
 require("./lib/updateProduct");
+require("./lib/readOneProduct");
