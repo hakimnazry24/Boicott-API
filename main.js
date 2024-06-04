e807e5023f4a7d8ff397b4cd48487ee3a411de0c
hakimnazry <hakimnazry@gmail.com>
2024-06-05 01:12:19 +0800

    cange port number

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const path = require("path");
 const express = require("express");
 const app = require("./routes/app");
-const PORT = 8080;
+const PORT = 3000;
 
 require("./middleware");
 require("./routes/routes");
